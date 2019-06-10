613c214c92673306672217da13d11b203f2a5a14
Emanuel Aguilar <Emanuel Aguilar@DESKTOP-M6GR8FE>
2019-06-10 13:21:19 -0600

    Finalizacion
    Final

---
+++ Examen_Emanuel_Aguilar/Examen_Emanuel_Aguilar/Clase_Empleado.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_Emanuel_Aguilar
+{
+    class Clase_Empleado
+    {
+        string nombre_empleado;
+        string identidad;
+        int horas_trabajadas;
+        float pago_por_hora_normal_del_trabajo;
+        int horas_extras_trabajadas;
+        float pago_por_hora_de_trabajo_extra;
+
+        public  Clase_Empleado()
+        {
+            nombre_empleado = "Emanuel Aguilar";
+            identidad = "1208-2000-00239";
+            horas_trabajadas = 5;
+            pago_por_hora_normal_del_trabajo = 1200;
+            horas_extras_trabajadas = 5;
+            pago_por_hora_de_trabajo_extra = 1200;
+        }
+
+        public string Nombre_Empleado
+        {
+            get
+            {
+                return nombre_empleado;
+            }
+            set
+            {
+                nombre_empleado = value;
+            }
+        }
+
+        public string Identidad
+        {
+            get
+            {
+                return identidad;
+            }
+            set
+            {
+                identidad = value;
+            }
+        }
+
+        public int Horas_Trabajadas
+        {
+            get
+            {
+                return horas_trabajadas;
+            }
+            set
+            {
+                horas_trabajadas = value;
+            }
+        }
+
+        public float Pago_Por_Hora_Normal_Del_Trabajo
+        {
+            get
+            {
+                return pago_por_hora_normal_del_trabajo;
+            }
+            set
+            {
+                pago_por_hora_normal_del_trabajo = value;
+            }
+        }
+
+        public int Horas_Extras_Trabajadas
+        {
+            get
+            {
+                return horas_extras_trabajadas;
+            }
+            set
+            {
+                horas_extras_trabajadas = value;
+            }
+        }
+
+        public float Pago_Por_Hora_De_Trabajo_Extra
+        {
+            get
+            {
+                return pago_por_hora_de_trabajo_extra;
+            }
+            set
+            {
+                pago_por_hora_de_trabajo_extra = value;
+            }
+        }
+
+
+        public void Nom()
+        {
+            string.Format(Nombre_Empleado);
+        }
+
+        public void Iden()
+        {
+            string.Format(Identidad);
+        }
+
+    }
+}
---
+++ Examen_Emanuel_Aguilar/Examen_Emanuel_Aguilar/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Examen_Emanuel_Aguilar
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    
+    {
+      
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string nombre = txtNombreEmpleado.Text;
+            string id = txtIdentidad.Text;
+            decimal dedu = (Convert.ToDecimal(txtHorasTrabajadas.Text) * Convert.ToDecimal(txtPagoPorHora.Text));
+            decimal ex = (Convert.ToDecimal(txtHorasExtras.Text) * Convert.ToDecimal(txtPagoHorasExtras.Text));
+            decimal IH = (((Convert.ToDecimal(txtHorasTrabajadas.Text) * Convert.ToDecimal(txtPagoPorHora.Text)) * Convert.ToDecimal(0.04)));
+            decimal RA = (((Convert.ToDecimal(txtHorasTrabajadas.Text) * Convert.ToDecimal(txtPagoPorHora.Text)) * Convert.ToDecimal(0.035)));
+            decimal PT = ((dedu+ex)-(IH+RA));
+            MessageBox.Show("Nombre: "+nombre+"\nIdentidad: "+id+"\nSueldo sin deducciones: "+dedu+"\nSueldo Horas Extras: "+ex+"\nDeducciones IHSS: "+IH+ "\nDeducciones RAP: " + RA + "\nPago Total: "+ PT);
+        }
+    }
+}
